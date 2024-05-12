1176926582a7645182a4fc2fdd08db79e47870e9
Ivan Avdyukov <vanya.avdyukov@yandex.ru>
2024-05-12 15:15:09 +0300

    add user

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+import java.util.Date;
+
+public class User {
+    String login;
+    String password;
+    Boolean sex;
+    Date birth;
+    Boolean isAdmin;
+}
