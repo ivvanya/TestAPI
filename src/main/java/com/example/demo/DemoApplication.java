541af3feed4204fdb1923b8386ad64d51e2c1f0b
Ivan Avdyukov <vanya.avdyukov@yandex.ru>
2024-05-12 16:18:28 +0300

    bd ready

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+//		SessionFactory sessionFactory = new Configuration().addAnnotatedClass(User.class).buildSessionFactory();
+//		Session session = sessionFactory.getCurrentSession();
+//		session.beginTransaction();
+//		User user = new User(1, "admin", "admin", true, new Date("01-01-2000"), true);
+//		session.save(user);
+//		session.getTransaction().commit();
+//		session.close();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User user = userRepository.findById(1).orElse(null);
+		System.out.println(user);
+	}
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,11 +1,34 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user", schema = "public")
+
 public class User {
-    String login;
-    String password;
-    Boolean sex;
-    Date birth;
-    Boolean isAdmin;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+    private String login;
+    private String password;
+    private Boolean sex;
+    private Date birth;
+    @Column(name = "is_admin")
+    private Boolean isAdmin;
+
+    @Override
+    public String toString() {
+        return login + " " + password;
+    }
 }
